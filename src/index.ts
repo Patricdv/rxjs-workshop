0c5ae8c895ac7a671871cf0d9a1e5d8f438379b4
Patric Venturini <patric.venturini@sky.uk>
2022-08-16 17:35:22 +0100

    Added basic rxjs operations

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,63 @@
-import { fromEvent } from "rxjs";
+import { debounce, scan, filter, interval, fromEvent, merge, Subject, switchMap } from "rxjs";
 
-fromEvent(document, "click")
-  .subscribe((event: PointerEvent) => {
-    const redDot = document.createElement("div");
-    redDot.style.position = "absolute";
-    redDot.style.background = "red";
-    redDot.style.width = "3px";
-    redDot.style.height = "3px";
-    redDot.style.borderRadius = "3px";
-    redDot.style.left = `${event.x}px`;
-    redDot.style.top = `${event.y}px`;
+const todoArray: any[] = [];
+const todoSubject = new Subject();
+const todo$ = todoSubject.asObservable();
 
-    document.body.append(redDot);
+const temporaryTodoSubject = new Subject();
+const temporaryTodo$ = temporaryTodoSubject.asObservable();
+
+const taskInput = document.querySelector("#task-input") as any;
+const megablasterButton = document.querySelector('#megablaster-button');
+const temporaryCard = document.querySelector('#temporary-card');
+
+const keydownEvent$ = fromEvent(taskInput, "keydown");
+const megablasterButton$ = fromEvent(megablasterButton, "click");
+const clickEvent$ = fromEvent(document, "click");
+
+keydownEvent$.pipe(debounce(() => interval(100))).subscribe(() => {
+  temporaryTodoSubject.next(taskInput.value)
+});
+
+merge(
+  keydownEvent$.pipe(filter((event: KeyboardEvent) => event.key === "Enter")),
+  megablasterButton$
+)
+  .subscribe(() => {
+  if (taskInput.value) {
+    todoArray.push(taskInput.value);
+    taskInput.value = "";
+    temporaryTodoSubject.next("");
+    todoSubject.next(todoArray);
+  }
+});
+
+clickEvent$.subscribe((event: any) => {
+  if (event.target.className === "card") {
+    todoArray.splice(parseInt(event.target.dataset.index, 10), 1);
+    todoSubject.next(todoArray);
+  }
+})
+
+todo$.subscribe((todoList: string[]) => {
+  document.querySelector("#list").innerHTML = "";
+
+  todoList.forEach((todo, index) => {
+    const todoCard = document.createElement("div");
+    todoCard.innerText = todo;
+    todoCard.className = "card";
+    todoCard.dataset.index = `${index}`;
+    document.querySelector("#list").prepend(todoCard);
   });
+});
 
-// TODO: Create Listener for add new Info to array (onKeyPress = Enter && onClick on button);
-
-// TODO: Create Subject and Observe it to dynamic generate a TODO list;
-
-// TODO: Create Debounce for temporary card;
+temporaryTodo$.subscribe((temporary: string) => {
+  if (temporary) {
+    temporaryCard.className = "show";
+    temporaryCard.innerHTML = temporary;
+    return;
+  }
+  temporaryCard.className = "";
+});
 
 // TODO: Erase elements from the list;
